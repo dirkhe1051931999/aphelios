8efb324469d69b8867c2cdce62259d69c3d37c17
dirkhe1051931999 <hejiandirk@163.com>
2024-03-01 14:25:12 +0800

    指定nodejs版本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
       autorestart: true, // 应用崩溃后自动重启
       args: 'start', // 启动命令
       script: './node_modules/nuxt/bin/nuxt.js', // Nuxt 启动脚本
+      interpreter: '/root/.nvm/versions/node/v18.16.0/bin/node', // 使用 Babel 解释器
       env: {
         NODE_ENV: 'production', // 设置环境变量
         HOST: '0.0.0.0', // 应用监听的主机地址
