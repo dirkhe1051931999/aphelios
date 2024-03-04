2450fdcbb77b16b9523d4eb0b85afb2d8688777e
dirkhe1051931999 <hejiandirk@163.com>
2024-03-04 14:42:01 +0800

    update

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 你的错误页面内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  mounted() {
+    // 检查是否因为页面不存在（404 错误）而渲染了错误页面
+    if (this.error.statusCode === 404) {
+      // 重定向到根路径
+      this.$router.replace('/');
+    }
+  },
+};
+</script>
